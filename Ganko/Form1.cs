746908d4abc9e1caf8ead2929292bacc23eeface
Morink <876886131@qq.com>
2018-06-21 14:41:59 +0800

    modofy

--- Ganko/Form1.cs
+++ Ganko/Form1.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin.Controls;
 
 namespace Ganko
 {
-    public partial class Form1 : Form
+    public partial class Ganko : MaterialForm
     {
-        public Form1()
+        public Ganko()
         {
             InitializeComponent();
         }
